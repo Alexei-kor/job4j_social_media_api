353e30011117d263f83501f5933ec91c756b7dc7
Alexei <aykornjushenkov@gmail.com>
2026-03-07 11:45:13 +0300

    Доменная модель [#505091]

---
+++ src/main/resources/db/changelog/v1/changelog-1.sql
@@ -0,0 +1,66 @@
+--liquibase formatted sql
+
+--changeset UserN:1
+--comment first
+create table users
+(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    email VARCHAR(100),
+    password VARCHAR(30)
+);
+
+create table posts
+(
+    id SERIAL PRIMARY KEY,
+    owner INT REFERENCES users(id),
+    date date,
+    time time,
+    head VARCHAR(100),
+    text text
+);
+
+create table images
+(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    ext VARCHAR(10),
+    image VARCHAR(1)
+);
+
+create table imagesPost
+(
+    id SERIAL PRIMARY KEY,
+    post_id INT REFERENCES posts(id),
+    image_id INT REFERENCES images(id)
+);
+
+create table requests
+(
+    id SERIAL PRIMARY KEY,
+    from_id INT REFERENCES users(id),
+    to_id INT REFERENCES users(id),
+    status INT
+);
+
+create table subscribers
+(
+    id SERIAL PRIMARY KEY,
+    owner_id INT REFERENCES users(id),
+    subscriber_id INT REFERENCES users(id)
+);
+
+create table friends
+(
+    id SERIAL PRIMARY KEY,
+    owner_id INT REFERENCES users(id),
+    friend_id INT REFERENCES users(id)
+);
+
+create table messages
+(
+    id SERIAL PRIMARY KEY,
+    from_id INT REFERENCES users(id),
+    to_id INT REFERENCES users(id),
+    text text
+);
