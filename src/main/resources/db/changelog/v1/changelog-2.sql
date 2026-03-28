5635aac71b0bf74113fb8560375726abbe1b3046
Alexei <aykornjushenkov@gmail.com>
2026-03-28 16:04:46 +0300

    8. DTO [#505114]

--- src/main/resources/db/changelog/v1/changelog-2.sql
+++ src/main/resources/db/changelog/v1/changelog-2.sql
@@ -1,20 +1,8 @@
 --liquibase formatted sql
 
---changeset UserN:2 splitStatements:false endDelimiter:@@
+--changeset UserN:2
 --comment first
-DO $$
-BEGIN
-    -- Проверяем существование ограничения в системном каталоге pg_constraint
-    IF NOT EXISTS (
-        SELECT 1
-        FROM pg_constraint
-        WHERE conname = 'useremail_unique'   -- Имя ограничения
-          AND conrelid = 'users'::regclass    -- Имя таблицы
-    ) THEN
-        -- Если ограничение не найдено, добавляем его
-        EXECUTE 'ALTER TABLE users ADD CONSTRAINT useremail_unique UNIQUE (email)';
-    END IF;
-END;
-$$;
-@@
+-- ALTER TABLE users ADD CONSTRAINT useremail_unique UNIQUE (email)
+
+
 
